78c761205a4e3de16890b1334f0389ac6c72d38c
Tristan7122 <154232254+Tristan7122@users.noreply.github.com>
2025-06-25 18:51:56 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'QUEEN-ELISA~',
+SESSION_ID: process.env.SESSION_ID || 'QUEEN-ELISA~fckViTRI#qsSAkd7aON6dCLTOEicd3tIe5D1aU5Ik5VhvFlmlkiI',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgresql://sadiya:MTCV3kmoO4YSt6bcK8naY9WCRRO7wL2v@dpg-d07n7k2li9vc73ff97bg-a/sula_md_db',
 LANG: process.env.BOT_LANG || 'EN' ,
-PREFIX: process.env.PREFIX || '#',
-ANTI_BAD: process.env.ANTI_BAD || 'false',
-MAX_SIZE: process.env.MAX_SIZE || 300,
-ONLY_GROUP: process.env.ONLY_GROUP || 'false',
-ANTI_LINK: process.env.ANTI_LINK || 'false' ,
+PREFIX: process.env.PREFIX || '.',
+ANTI_BAD: process.env.ANTI_BAD || 'true',
+MAX_SIZE: process.env.MAX_SIZE ||900,
+ONLY_GROUP: process.env.ONLY_GROUP || 'true',
+ANTI_LINK: process.env.ANTI_LINK || 'true' ,
 ANTI_BOT: process.env.ANTI_BOT || 'false',
 ALIVE: process.env.ALIVE || `default`,
 FOOTER: process.env.FOOTER ||  '©QUEEN - ELISA - MD',
